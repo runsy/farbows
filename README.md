80330cd94995db656f8f1fdb10abacc410c8d664
runs <runs@runs-PC.runs.es>
2020-04-04 18:11:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+ Minetest mod: farbows
+=======================
+Just like all the other bow mods but this time with Raycast.
+Not compatible with any other bow mod.
+
+Depends: default, farming
+
+
+ Features
+----------
+
+- Place/Rightclick to charge
+- Punch/Leftclick to fire
+
+ Licenses
+----------
+GPL 3.0 for everything.
---
+++ init.lua
@@ -0,0 +1,51 @@
+farbows = {}
+
+-- internationalization boilerplate
+local S = minetest.get_translator(minetest.get_current_modname())
+
+rcbows.register_arrow("farbows:e_arrow", {
+	projectile_texture = "farbows_proyectile_arrow",
+	damage = 5,
+	inventory_arrow = {
+		name = "farbows:inv_arrow",
+		description = S("Arrow"),
+		inventory_image = "farbows_arrow.png",
+	}
+})
+
+rcbows.register_bow("farbows:bow_wood", {
+	description = S("Wooden Bow"),
+	image = "farbows_bow_wood.png",
+	strength = 30,
+	uses = 150,
+	recipe = {
+		{"", "group:wood", "farming:string"},
+		{"group:wood", "", "farming:string"},
+		{"", "group:wood", "farming:string"},
+	},
+	overlay_empty = "farbows_overlay_empty.png",
+	overlay_charged = "farbows_overlay_charged.png",
+	arrow = "farbows:e_arrow",
+})
+
+rcbows.register_bow("farbows:bow_mese", {
+	description = S("Mese Bow"),
+	image = "farbows_bow_mese.png",
+	strength = 60,
+	uses = 800,
+	recipe = {
+		{"", "default:mese_crystal", "farming:string"},
+		{"default:mese_crystal", "", "farming:string"},
+		{"", "default:mese_crystal", "farming:string"},
+	},
+	overlay_empty = "farbows_overlay_empty.png",
+	overlay_charged = "farbows_overlay_charged.png",
+	arrow = "farbows:e_arrow",
+})
+
+minetest.register_craft({
+	output = "farbows:e_arrow 5",
+	recipe = {
+		{"default:steel_ingot", "default:stick", "default:stick"},
+	}
+})
