6f6e3280a3248c502265fa5886ead2492fa991dd
runs <runs@runs-PC.runs.es>
2020-04-05 03:34:56 +0200

    fire arrow

--- init.lua
+++ init.lua
@@ -10,6 +10,41 @@ rcbows.register_arrow("farbows:e_arrow", {
 		name = "farbows:inv_arrow",
 		description = S("Arrow"),
 		inventory_image = "farbows_arrow.png",
+	},
+})
+
+minetest.register_craft({
+	output = "farbows:inv_arrow 5",
+	type = "shaped",
+	recipe = {
+		{"", "", "default:steel_ingot"},
+		{"", "default:stick", ""},
+		{"farming:cotton", "", ""},
+	}
+})
+
+rcbows.register_arrow("farbows:fire_arrow", {
+	projectile_texture = "farbows_proyectile_arrow",
+	damage = 7,
+	inventory_arrow = {
+		name = "farbows:inv_fire_arrow",
+		description = S("Fire Arrow"),
+		inventory_image = "farbows_arrow_fire.png",
+	},
+	drop = "farbows:inv_arrow",
+	effects = {
+		replace_node = "fire:basic_flame",
+		trail_particle = "farbows_particle_fire.png",
+	}
+})
+
+minetest.register_craft({
+	output = "farbows:inv_fire_arrow 2",
+	type = "shaped",
+	recipe = {
+		{"", "", "default:steel_ingot"},
+		{"", "default:torch", ""},
+		{"farming:cotton", "", ""},
 	}
 })
 
@@ -51,9 +86,21 @@ rcbows.register_bow("farbows:bow_mese", {
 	}
 })
 
-minetest.register_craft({
-	output = "farbows:e_arrow 5",
+rcbows.register_bow("farbows:bow_flaming", {
+	description = S("Flaming Far Bow"),
+	image = "farbows_bow_flaming.png",
+	strength = 100,
+	uses = 1500,
 	recipe = {
-		{"default:steel_ingot", "default:stick", "default:stick"},
+		{"", "default:obsidian_shard", "farming:string"},
+		{"default:gold_lump", "", "farming:string"},
+		{"", "default:obsidian_shard", "farming:string"},
+	},
+	overlay_empty = "farbows_overlay_empty.png",
+	overlay_charged = "farbows_overlay_flaming_charged.png",
+	arrow = "farbows:fire_arrow",
+	sounds = {
+		max_hear_distance = 10,
+		gain = 0.4,
 	}
 })
